b7c7a15dceef8a6cd0078d871d65c304f28e14f7
Lucas Barrozo <lucaslbarrozo@gmail.com>
2020-06-14 13:36:37 -0300

    configuracao basica de css e do ejs

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.get('/',(req, res) => {
-    res.send('Hello World!')
+app.use("/static", express.static("public"))
+app.set("view engine", "ejs")
+
+
+//config do view engine
+app.get('/', (req, res) => {
+    res.render('todo.ejs')
 })
+
 
 app.listen(3000, () => console.log("Servidor Funcionando"))
