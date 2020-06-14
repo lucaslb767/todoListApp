e40e8213bc4cf4b7b06c53d69e585628dea3ef50
Lucas Barrozo <lucaslbarrozo@gmail.com>
2020-06-14 14:39:22 -0300

    conecta com o mongodb

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express = require('express')
 const app = express()
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+
+dotenv.config()
 
 app.use("/static", express.static("public"))
-app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }))
 
+
+//connection to db
+mongoose.set("useFindAndModify", false)
+
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+    console.log("Connected to db!");
+    app.listen(3000, () => console.log("Servidor Funcionando"));
+})
 
 //config do view engine
+app.set("view engine", "ejs")
+
+//get
 app.get('/', (req, res) => {
     res.render('todo.ejs')
 })
 
-
-app.listen(3000, () => console.log("Servidor Funcionando"))
+//post
+app.post('/', (req, res) => {
+    console.log(req.body);
+})
